59a34bdcfaae7716fd1296c88b9df9f46316cd08
Wilian Pereira <wilian_pereira92@outlook.com>
2022-04-11 19:29:01 -0300

    feat: adicionando os estados guessedLetters, wrongLetters, guesses, score

--- src/App.js
+++ src/App.js
@@ -19,6 +19,10 @@ function App() {
   const [pickedCategory, setPickedCategory] = useState("");
   const [letters, setLetters] = useState([]);
 
+  const [guessedLetters, setGuessedLetters] = useState([]); 
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [guesses, setGuesses] = useState(3);
+  const [score, setScore] = useState(0);
 
   const pickWordAndCategory = () => {
     const categories = Object.keys(words);
@@ -37,7 +41,7 @@ function App() {
     wordLetters = wordLetters.map((l) => l.toLowerCase());
     setPickedWord(word);
     setPickedCategory(category)
-    setLetters(letters)
+    setLetters(wordLetters)
 
     setGameStage(stages[1].name);
   }
@@ -53,7 +57,17 @@ function App() {
   return (
     <div className="App">
       {gameStage === 'start' && <StartScreen startGame={startGame} />}
-      {gameStage === 'game' && <Game verifyLetter={verifyLetter}/>}
+      {gameStage === 'game' && (
+      <Game 
+        verifyLetter={verifyLetter} 
+        pickedWord={pickedWord} 
+        pickedCategory={pickedCategory}
+        letters={letters}
+        guessedLetters={guessedLetters}
+        wrongLetters={wrongLetters}
+        guesses={guesses}
+        score={score}
+      />)}
       {gameStage === 'end' && <GameOver retry={retry} />}
     </div>
   );
