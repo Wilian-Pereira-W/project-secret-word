d4e889a56e30b73ab240402e98a249922b7ac78e
Wilian Pereira <wilian_pereira92@outlook.com>
2022-04-11 18:39:14 -0300

    feat: implementando o componente game

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -3,14 +3,31 @@ import styles from './styles.module.scss'
 
 function Game({ verifyLetter } ) {
   return (
-    <div>
-      <h1>Game</h1>
-      <button
-        type="button"
-        onClick={verifyLetter}
-      >
-        Finalizar jogo
-      </button>
+    <div className={styles.game}>
+      <p className={styles.points}>
+        <span>Pontuação: 000</span>
+      </p>
+      <h1>Adivinhe a palavra:</h1>
+      <h3 className={styles.trip}>
+        Dica sobre a palavra: <span>Dica...</span>
+      </h3>
+      <div className={styles.wordContainer}>
+        <span className={styles.letter}>A</span>
+        <span className={styles.blankSquare}></span>
+      </div>
+      <div>
+        <p>Tente adivinhar uma letra da palavra:</p>
+        <form>
+          <input type="text" name="letter" maxLength="1" required/>
+          <button type="button">Jogar</button>
+        </form>
+      </div>
+      <div className={styles.wrongLettersContainer}>
+        <p>Letras já utilizadas:</p>
+        <span>a,</span>
+
+      </div>
+
     </div>
   );
 }
