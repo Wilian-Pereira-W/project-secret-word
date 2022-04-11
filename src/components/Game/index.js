8130045e1823cdaf8e0f06739d44df73f43403b3
Wilian Pereira <wilian_pereira92@outlook.com>
2022-04-11 19:27:40 -0300

    feat: recebendo as props

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import styles from './styles.module.scss'
 
-function Game({ verifyLetter } ) {
+function Game({ verifyLetter, pickedWord, pickedCategory, letters, guessedLetters, wrongLetters, guesses, score } ) {
   return (
     <div className={styles.game}>
       <p className={styles.points}>
-        <span>Pontuação: 000</span>
+        <span>Pontuação: {score}</span>
       </p>
       <h1>Adivinhe a palavra:</h1>
       <h3 className={styles.trip}>
-        Dica sobre a palavra: <span>Dica...</span>
+        Dica sobre a palavra: <span>{pickedCategory}</span>
       </h3>
+      <p>Você ainda tem {guesses} tentativas</p>
       <div className={styles.wordContainer}>
-        <span className={styles.letter}>A</span>
-        <span className={styles.blankSquare}></span>
+        {letters.map((letter, index) => (
+          guessedLetters.includes(letter) ? (
+            <span key={index} className={styles.letter}>{letter}</span>
+          ) :
+          (
+          <span keys={index} className={styles.blankSquare}></span>
+          )
+        ))}
       </div>
-      <div>
+      <div className={styles.letterContainer}>
         <p>Tente adivinhar uma letra da palavra:</p>
         <form>
           <input type="text" name="letter" maxLength="1" required/>
@@ -24,8 +31,9 @@ function Game({ verifyLetter } ) {
       </div>
       <div className={styles.wrongLettersContainer}>
         <p>Letras já utilizadas:</p>
-        <span>a,</span>
-
+        {wrongLetters.map((letter, index) => (
+          <span key={index}>{letter}, </span>
+        ))}
       </div>
 
     </div>
