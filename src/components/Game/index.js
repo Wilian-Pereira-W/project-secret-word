c31ce23c58ed72dbca26126947d01701f495af16
Wilian Pereira <wilian_pereira92@outlook.com>
2022-04-11 17:29:02 -0300

    feat: importando a função verifyLetter

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import styles from './styles.module.scss'
 
-function Game() {
+function Game({ verifyLetter } ) {
   return (
     <div>
-      
+      <h1>Game</h1>
+      <button
+        type="button"
+        onClick={verifyLetter}
+      >
+        Finalizar jogo
+      </button>
     </div>
   );
 }
