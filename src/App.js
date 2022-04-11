477725a1db19f92831589c250177eb737fe11bc3
Wilian Pereira <wilian_pereira92@outlook.com>
2022-04-11 17:15:51 -0300

    feat: importado o data e os componentes Game e GameOver

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import './App.css';
+import { wordsList } from './data/words'
 import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+  {id: 1, name: 'start'},
+  {id: 2, name: 'game'},
+  {id: 3, name: 'end'},
+]
 
 function App() {
+  const [gameStage, setGameStage] = useState('start');
+  const [words] = useState(wordsList);
+  console.log(words)
   return (
     <div className="App">
-     <StartScreen />
+      {gameStage === 'start' && <StartScreen />}
+      {gameStage === 'game' && <Game />}
+      {gameStage === 'end' && <GameOver />}
     </div>
   );
 }
