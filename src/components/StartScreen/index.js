3d8591422703fb1be0f0e807f801d6abcbc2637e
Wilian Pereira <wilian_pereira92@outlook.com>
2022-04-11 17:28:16 -0300

    feat: implementando a função de onClick

--- src/components/StartScreen/index.js
+++ src/components/StartScreen/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import styles from './styles.module.scss'
 
-function StartScreen() {
+function StartScreen( { startGame } ) {
   return (
     <div className={styles.start}>
       <h1>Secret Word</h1>
       <p>Clique no botão abaixo para começar a jogar</p>
-      <button>Começar o jogo</button>
+      <button
+        type="button"
+        onClick={startGame}
+      >
+        Começar o jogo
+      </button>
     </div>
   );
 }
