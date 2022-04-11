f9c9af1eb774ca7b175400da5f06eba18cdec914
Wilian Pereira <wilian_pereira92@outlook.com>
2022-04-11 17:27:41 -0300

    feat: implementando as funções startGame e  verifyLetter

--- src/App.js
+++ src/App.js
@@ -12,13 +12,22 @@ const stages = [
 ]
 
 function App() {
-  const [gameStage, setGameStage] = useState('start');
+  const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
-  console.log(words)
+  
+
+  const startGame = () => {
+    setGameStage(stages[1].name);
+  }
+
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  }
+
   return (
     <div className="App">
-      {gameStage === 'start' && <StartScreen />}
-      {gameStage === 'game' && <Game />}
+      {gameStage === 'start' && <StartScreen startGame={startGame} />}
+      {gameStage === 'game' && <Game verifyLetter={verifyLetter}/>}
       {gameStage === 'end' && <GameOver />}
     </div>
   );
