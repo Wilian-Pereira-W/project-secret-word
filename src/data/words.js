30570ad694fc38ac9b91f5d09c066d4c5f81240c
Wilian Pereira <wilian_pereira92@outlook.com>
2022-04-12 18:56:40 -0300

    feat: Adicionando mais palavras

--- src/data/words.js
+++ src/data/words.js
@@ -1,8 +1,10 @@
 export const wordsList = {
   carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
-  fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
-  corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
-  computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+  fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Uva", "Cajá", "Pinha"],
+  corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos", "Cabelo", "Nariz"],
+  computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "HD", "SSD"],
   programação: ["Linguagem", "Framework", "JavaScript", "React"],
-  alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+  alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo", "Pão"],
+  cor: ["Abóbora", "Amarelo", "Azul", "Caramelo", "Gelo", "Marrom", "Roxo", "Rosa", "Vermelho", "Verde"],
+  animal: ["Gato", "Pato", "Galo", "Galinha", "Marreco", "Ganso", "Peixe", "Boi", "Vaca", "Cavalo"],
 };
