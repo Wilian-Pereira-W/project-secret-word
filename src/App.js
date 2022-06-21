ad0910ae028e199d2d676feb300b4618a9c23931
Wilian Pereira <wilian_pereira92@outlook.com>
2022-06-21 10:30:27 -0300

    feat: adicionando a rota Login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './components/Home';
+import SignIn from './components/SignIn';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
