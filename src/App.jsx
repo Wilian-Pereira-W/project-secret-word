24bd6e284c4e0d3042d712b657d9aea548e3d2c5
Wilian Pereira <wilian_pereira92@outlook.com>
2022-06-21 22:11:31 -0300

    feat: alterando estensão para jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './components/Home';
 import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<SignIn />} />
+        <Route path="/register" element={<SignUp />} />
         <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
