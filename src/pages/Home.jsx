01f3e1ec922986e88d93162371526007a3579328
Wilian Pereira <wilian_pereira92@outlook.com>
2022-06-27 21:14:43 -0300

    feat: implemetando Page Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import StartGame from '../components/StartGame';
+
+function Home() {
+  return (
+    <div>
+      <StartGame />
+    </div>
+  );
+}
+
+export default Home;
