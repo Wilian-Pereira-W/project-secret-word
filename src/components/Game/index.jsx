d56c6bcd7f72d0e9b15bef2bbad75ca8ad45c17a
Wilian Pereira <wilian_pereira92@outlook.com>
2022-06-27 21:03:53 -0300

    fix: passando para o provider os estados

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -1,18 +1,19 @@
 import PropTypes from 'prop-types';
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useContext } from 'react';
+import AppContext from '../../context/context';
 import styles from './styles.module.scss';
 
-function Game({
-  verifyLetter,
-  pickedCategory,
-  letters,
-  guessedLetters,
-  wrongLetters,
-  guesses,
-  score,
-}) {
+function Game({ verifyLetter }) {
   const [letter, setLetter] = useState('');
   const letterInputRef = useRef(null);
+  const {
+    letters,
+    guessedLetters,
+    guesses,
+    wrongLetters,
+    pickedCategory,
+    score,
+  } = useContext(AppContext);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -23,65 +24,56 @@ function Game({
   };
 
   return (
-    <div className={styles.game}>
-      <p className={styles.points}>
-        <span>Pontuação: {score}</span>
-      </p>
-      <h1>Adivinhe a palavra:</h1>
-      <h3 className={styles.trip}>
-        Dica sobre a palavra: <span>{pickedCategory}</span>
-      </h3>
-      <p>Você ainda tem {guesses} tentativas</p>
-      <div className={styles.wordContainer}>
-        {letters.map((letter, index) =>
-          guessedLetters.includes(letter) ? (
-            <span key={index} className={styles.letter}>
-              {letter}
-            </span>
-          ) : (
-            <span keys={index} className={styles.blankSquare}></span>
-          ),
-        )}
+    <main className={styles.container}>
+      <div className={styles.game}>
+        <h1>Adivinhe a palavra:</h1>
+        <p className={styles.points}>
+          <span>Pontuação: {score}</span>
+        </p>
+        <h3 className={styles.trip}>
+          Dica sobre a palavra: <span>{pickedCategory}</span>
+        </h3>
+        <p>Você ainda tem {guesses} tentativas</p>
+        <div className={styles.wordContainer}>
+          {letters.map((letter, index) =>
+            guessedLetters.includes(letter) ? (
+              <span key={`${index}`} className={styles.letter}>
+                {letter}
+              </span>
+            ) : (
+              <span key={`${index * 12}`} className={styles.blankSquare}></span>
+            ),
+          )}
+        </div>
+        <div className={styles.letterContainer}>
+          <p>Tente adivinhar uma letra da palavra:</p>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="letter"
+              maxLength="1"
+              required
+              onChange={({ target }) => setLetter(target.value)}
+              value={letter}
+              ref={letterInputRef}
+            />
+            <button>Jogar</button>
+          </form>
+        </div>
+        <div className={styles.wrongLettersContainer}>
+          <p>Letras já utilizadas:</p>
+          {wrongLetters.map((letter, index) => (
+            <span key={`${index * 13}`}>{letter}, </span>
+          ))}
+        </div>
       </div>
-      <div className={styles.letterContainer}>
-        <p>Tente adivinhar uma letra da palavra:</p>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="letter"
-            maxLength="1"
-            required
-            onChange={({ target }) => setLetter(target.value)}
-            value={letter}
-            ref={letterInputRef}
-          />
-          <button>Jogar</button>
-        </form>
-      </div>
-      <div className={styles.wrongLettersContainer}>
-        <p>Letras já utilizadas:</p>
-        {wrongLetters.map((letter, index) => (
-          <span key={index}>{letter}, </span>
-        ))}
-      </div>
-    </div>
+    </main>
   );
 }
 
 Game.propTypes = {
-  guessedLetters: PropTypes.shape({
-    includes: PropTypes.func,
-  }),
   guesses: PropTypes.any,
-  letters: PropTypes.shape({
-    map: PropTypes.func,
-  }),
-  pickedCategory: PropTypes.any,
-  score: PropTypes.string,
   verifyLetter: PropTypes.func,
-  wrongLetters: PropTypes.shape({
-    map: PropTypes.func,
-  }),
 };
 
 export default Game;
