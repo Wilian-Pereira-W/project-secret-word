c29a02a1e4abb2dbfc95d5f20b610196c7eaeaf1
Wilian Pereira <wilian_pereira92@outlook.com>
2022-06-28 19:26:13 -0300

    refactor: removendo  headers

--- src/services/api.js
+++ src/services/api.js
@@ -2,9 +2,6 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_HOST,
-  headers: {
-    'Content-Type': 'application/json',
-  },
 });
 
 export default api;
