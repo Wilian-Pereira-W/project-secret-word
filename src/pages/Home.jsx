902d67decc40c100babffb11128b0ffbacef45ec
Wilian Pereira <wilian_pereira92@outlook.com>
2022-06-28 18:55:04 -0300

    feat: validar rota home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import StartGame from '../components/StartGame';
+import api from '../services/api';
+import { getLocalStorage } from '../utils/localStorage';
 
 function Home() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const user = getLocalStorage('user');
+    const config = {
+      headers: { Authorization: `Bearer ${user.access_token}` },
+    };
+    api
+      .get('me', config)
+      .then()
+      .catch(() => {
+        navigate('/login');
+      });
+  }, [navigate]);
   return (
     <div>
       <StartGame />
